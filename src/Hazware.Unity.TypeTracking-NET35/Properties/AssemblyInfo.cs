566045aaaea6c9a94e8379c6f91d6cc54a62a1c7
dbuksbaum <david.buksbaum@gmail.com>
2012-05-18 22:07:54 -0400

    Modified assemblyinfo.cs information

--- src/Hazware.Unity.TypeTracking-NET35/Properties/AssemblyInfo.cs
+++ src/Hazware.Unity.TypeTracking-NET35/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Buksbaum")]
 [assembly: AssemblyProduct("Hazware.Unity.TypeTracking")]
-[assembly: AssemblyCopyright("Copyright © David Buksbaum 2009-2011")]
+[assembly: AssemblyCopyright("Copyright © David Buksbaum 2009-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -26,11 +26,11 @@ using System.Runtime.InteropServices;
 //
 //      Major Version
 //      Minor Version 
-//      Build Number
+//      Build NumberAssenbloyt
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/Hazware.Unity.TypeTracking-SL4/Properties/AssemblyInfo.cs
+++ src/Hazware.Unity.TypeTracking-SL4/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hazware.Unity.TypeTracking_SL3")]
+[assembly: AssemblyTitle("Hazware.Unity.TypeTracking_SL4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Hazware.Unity.TypeTracking_SL3")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyProduct("Hazware.Unity.TypeTracking_SL4")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
